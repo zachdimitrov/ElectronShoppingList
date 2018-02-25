d78920a90cb7a061e9c5eba874838d703fedaf6e
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2018-02-25 23:18:19 +0200

    add dev tools menu

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require("path");
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 // listen for app to bew ready
 
@@ -17,18 +18,49 @@ app.on("ready", () => {
         protocol: "file",
         slashes: true,
     }));
+    // quit app when closed
+    mainWindow.on("closed", () => {
+        app.quit();
+    });
 
     // build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
     // instert menu
     Menu.setApplicationMenu(mainMenu);
 });
 
+// handle create/add window
+function createAddWindow() {
+    // create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: "Add Shopping List Item",
+    });
+
+    // load html file
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "add-window.html"),
+        protocol: "file",
+        slashes: true,
+    }));
+
+    // garbage collection
+    addWindow.on("close", () => {
+        addWindow === null;
+    })
+}
+
 // create menu template
 const mainMenuTemplate = [{
     label: "File",
-    submenu: [
-        { label: "Add Item" },
+    submenu: [{
+            label: "Add Item",
+            click: () => {
+                createAddWindow();
+            }
+        },
         { label: "Clear Items" },
         {
             label: "Quit",
@@ -38,4 +70,25 @@ const mainMenuTemplate = [{
             }
         }
     ]
-}]
+}];
+
+// if mac, add empty object ot menu
+if (process.platform === "darwin") {
+    mainMenuTemplate.unshift({});
+}
+
+// add developer tools item if not production
+if (process.env.NODE_ENV !== "production") {
+    mainMenuTemplate.push({
+        label: "Open Developer Tools",
+        submenu: [{
+            label: "Toggle DevTools",
+            accelerator: process.platform == "darwin" ? "Command+D" : "Ctrl+D",
+            click: (item, focusedWindow) => {
+                focusedWindow.toggleDevTools();
+            }
+        }, {
+            role: "reload",
+        }]
+    })
+}
